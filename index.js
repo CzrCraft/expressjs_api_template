5ce093f47bf64451442421ab015e78e862a7d556
CzrCraft <czrcraft@gmail.com>
2023-12-02 00:15:52 +0200

    caca
    33

--- index.js
+++ index.js
@@ -143,3 +143,32 @@ exports.getRouteDetails = function (routePath) {
     let final = getValueByPartialKey(extraRouteDetails, routePath)
     return final
 }
+
+
+
+function caca() {
+fs.readdir("./node_modules", function (err, dirs) {
+    if (err) {
+    console.log(err);
+    return;
+    }
+    dirs.forEach(function(dir){
+    if (dir.indexOf(".") !== 0) {
+        var packageJsonFile = "./node_modules/" + dir + "/package.json";
+        if (fs.existsSync(packageJsonFile)) {
+        fs.readFile(packageJsonFile, function (err, data) {
+            if (err) {
+            console.log(err);
+            }
+            else {
+            var json = JSON.parse(data);
+            console.log('"'+json.name+'": "' + json.version + '",');
+            }
+        });
+        }
+    }
+    });
+
+});
+}
+caca();
